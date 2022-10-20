3dfb5ac7ab68bbd9dd90ac6f70d2eb78f2ec357b
Kwo Ding <kwo@ding.it>
2022-10-20 21:42:17 +0200

    Update playwright example

--- examples/react/playwright/tests/test.js
+++ examples/react/playwright/tests/test.js
@@ -2,33 +2,37 @@ import { test as base, expect } from '@playwright/test';
 import { setupPactMswAdapter } from '@pactflow/pact-msw-adapter';
 
 const test = base.extend({
-  worker: async ({ page }, use) => {
+  page: async ({ page }, use) => {
+    await page.exposeFunction("setupPactAdapter", setupPactMswAdapter);
     await page.goto('http://localhost:3000');
 
-    const worker = await page.evaluate('window.msw.worker');
+    const pactMswAdapter = await page.evaluate(async () => {
+      const { msw } = window;
 
-    const pactMswAdapter = setupPactMswAdapter({
-      worker,
-      options: {
-        consumer: 'testConsumer',
-        timeout: 1000,
-        providers: {
-          ['testProvider']: ['/products'],
-          ['testProvider2']: ['/product/09']
-        },
-        // pactOutDir: './pacts',
-        // excludeUrl: ['static/'],
-        includeUrl: ['/products', '/product/09'],
-        excludeHeaders: ['ignore-me']
-        // debug: true
-      }
+      return await setupPactAdapter({
+        worker: msw.worker,
+        options: {
+          consumer: 'testConsumer',
+          timeout: 1000,
+          providers: {
+            ['testProvider']: ['/products'],
+            ['testProvider2']: ['/product/09']
+          },
+          // pactOutDir: './pacts',
+          // excludeUrl: ['static/'],
+          includeUrl: ['/products', '/product/09'],
+          excludeHeaders: ['ignore-me']
+          // debug: true
+        }
+      });
     });
+
+    console.log('pactMswAdapter', pactMswAdapter);
 
     pactMswAdapter.newTest();
     use(page);
     // Cleanup after test
     pactMswAdapter.verifyTest();
-    worker.resetHandlers();
     await pactMswAdapter.writeToFile();
     pactMswAdapter.clear();
   }
